16c8c7641e8d23e6e562feb555b342253c487ba0
Tanushrik <tanushrik311@gmail.com>
2026-02-03 12:45:43 +0530

    AddCode

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -39,7 +39,7 @@ export default function TabTwoScreen() {
         </ThemedText>
         <ThemedText>
           The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
+          sets up the tab Navigator.
         </ThemedText>
         <ExternalLink href="https://docs.expo.dev/router/introduction">
           <ThemedText type="link">Learn more</ThemedText>
